e0e177f0d3abcf56678ae61980d9dd54daa3f5c0
duaa-braik <duaa13braik@gmail.com>
2023-03-05 15:32:51 +0200

    first commit

---
+++ DoctorWhoWebApi/Controllers/DoctorController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DoctorWhoWebApi.Controllers
+{
+    [ApiController]
+    [Route("/doctors")]
+    public class DoctorController : Controller
+    {
+        [HttpGet]
+        public IActionResult GetDoctors()
+        {
+            return Ok();
+        }
+    }
+}
